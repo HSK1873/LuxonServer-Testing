daa030760ffd9c7327d67113586d2472e97d4e2b
niansa <tuxifan@posteo.de>
2026-07-18 16:15:38 +0200

    Several memory management fixes in basiccoro

--- basic-coro/include/AwaitableTask.hpp
+++ basic-coro/include/AwaitableTask.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <atomic>
 #include <concepts>
 #include <coroutine>
 #include <exception>
+#include <optional>
 #include <stdexcept>
+#include <type_traits>
 #include <utility>
 
 namespace basiccoro {
@@ -12,21 +15,34 @@ namespace detail {
 template <class Derived> struct PromiseBase {
     std::exception_ptr exception_;
 
-    auto get_return_object() { return std::coroutine_handle<Derived>::from_promise(static_cast<Derived&>(*this)); }
-    void unhandled_exception() { exception_ = std::current_exception(); }
+    auto get_return_object() noexcept { return std::coroutine_handle<Derived>::from_promise(static_cast<Derived&>(*this)); }
+    void unhandled_exception() noexcept { exception_ = std::current_exception(); }
 };
 
-template <class Derived, class T>
-    requires std::movable<T> || std::same_as<T, void>
-struct ValuePromise : public PromiseBase<Derived> {
+template <class Derived, class T> struct ValuePromise : public PromiseBase<Derived> {
+    static_assert(std::movable<T>, "T must be movable");
+
     using value_type = T;
-    T val;
-    void return_value(T t) { val = std::move(t); }
+    std::optional<T> val_;
+
+    void return_value(T&& t) { val_.emplace(std::move(t)); }
+
+    void return_value(const T& t)
+        requires std::copy_constructible<T>
+    {
+        val_.emplace(t);
+    }
+
+    template <class U>
+        requires(!std::same_as<std::remove_cvref_t<U>, T> && std::constructible_from<T, U &&>)
+    void return_value(U&& u) {
+        val_.emplace(std::forward<U>(u));
+    }
 };
 
 template <class Derived> struct ValuePromise<Derived, void> : public PromiseBase<Derived> {
     using value_type = void;
-    void return_void() {}
+    void return_void() noexcept {}
 };
 
 template <class T> class AwaitablePromise : public ValuePromise<AwaitablePromise<T>, T> {
@@ -35,32 +51,50 @@ public:
 
     auto final_suspend() noexcept {
         struct FinalAwaiter {
-            std::coroutine_handle<> waiting;
-
             bool await_ready() const noexcept { return false; }
 
-            std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept {
-                if (waiting) {
-                    return waiting;
+            std::coroutine_handle<> await_suspend(std::coroutine_handle<AwaitablePromise> handle) noexcept {
+                auto& promise = handle.promise();
+                void *previous = promise.waiting_.exchange(completedState(), std::memory_order_acq_rel);
+
+                if (!previous || previous == completedState()) {
+                    return std::noop_coroutine();
                 }
-                return std::noop_coroutine();
+
+                return std::coroutine_handle<>::from_address(previous);
             }
 
             void await_resume() const noexcept {}
         };
 
-        return FinalAwaiter{waiting_};
+        return FinalAwaiter{};
     }
 
-    void storeWaiting(std::coroutine_handle<> handle) {
-        if (waiting_) {
-            throw std::runtime_error("AwaitablePromise::storeWaiting(): already waiting");
+    bool completed() const noexcept { return waiting_.load(std::memory_order_acquire) == completedState(); }
+
+    bool storeWaiting(std::coroutine_handle<> handle) {
+        if (!handle) {
+            throw std::runtime_error("AwaitablePromise::storeWaiting(): null handle");
         }
-        waiting_ = handle;
+
+        void *expected = nullptr;
+        void *waiting = handle.address();
+
+        if (waiting_.compare_exchange_strong(expected, waiting, std::memory_order_release, std::memory_order_acquire)) {
+            return true;
+        }
+
+        if (expected == completedState()) {
+            return false;
+        }
+
+        throw std::runtime_error("AwaitablePromise::storeWaiting(): already waiting");
     }
 
 private:
-    std::coroutine_handle<> waiting_ = nullptr;
+    static void *completedState() noexcept { return reinterpret_cast<void *>(1); }
+
+    std::atomic<void *> waiting_ = nullptr;
 };
 
 template <class Promise> class TaskBase {
@@ -93,9 +127,11 @@ public:
         }
     }
 
-    bool done() const { return !handle_ || handle_.done(); }
+    bool done() const noexcept { return !handle_ || handle_.done(); }
 
 protected:
+    std::coroutine_handle<promise_type> release() noexcept { return std::exchange(handle_, nullptr); }
+
     std::coroutine_handle<promise_type> handle_ = nullptr;
 };
 
@@ -105,31 +141,66 @@ template <class T> class [[nodiscard]] AwaitableTask : public detail::TaskBase<d
     using Base = detail::TaskBase<detail::AwaitablePromise<T>>;
 
 public:
+    using promise_type = typename Base::promise_type;
     using Base::Base;
 
-    class awaiter;
-    friend class awaiter;
-    awaiter operator co_await() const;
-};
+    class awaiter {
+    public:
+        explicit awaiter(std::coroutine_handle<promise_type> handle) noexcept : handle_(handle) {}
 
-template <class T> struct AwaitableTask<T>::awaiter {
-    bool await_ready() const noexcept { return task_.done(); }
+        awaiter(const awaiter&) = delete;
+        awaiter& operator=(const awaiter&) = delete;
+        awaiter& operator=(awaiter&&) = delete;
 
-    template <class Promise> void await_suspend(std::coroutine_handle<Promise> handle) { task_.handle_.promise().storeWaiting(handle); }
+        awaiter(awaiter&& other) noexcept : handle_(std::exchange(other.handle_, nullptr)) {}
 
-    T await_resume() {
-        if (task_.handle_.promise().exception_) {
-            std::rethrow_exception(task_.handle_.promise().exception_);
+        ~awaiter() {
+            if (handle_) {
+                handle_.destroy();
+            }
         }
 
-        if constexpr (!std::is_same_v<void, T>) {
-            return std::move(task_.handle_.promise().val);
+        bool await_ready() const noexcept { return !handle_ || handle_.promise().completed(); }
+
+        template <class Promise> bool await_suspend(std::coroutine_handle<Promise> handle) {
+            if (!handle_) {
+                throw std::runtime_error("AwaitableTask::awaiter::await_suspend(): no coroutine");
+            }
+
+            return handle_.promise().storeWaiting(handle);
         }
-    }
 
-    const AwaitableTask& task_;
+        T await_resume() {
+            if (!handle_) {
+                throw std::runtime_error("AwaitableTask::awaiter::await_resume(): no coroutine");
+            }
+
+            auto& promise = handle_.promise();
+
+            if (promise.exception_) {
+                std::rethrow_exception(promise.exception_);
+            }
+
+            if constexpr (!std::is_same_v<void, T>) {
+                if (!promise.val_) {
+                    throw std::runtime_error("AwaitableTask::awaiter::await_resume(): no value");
+                }
+
+                T value = std::move(*promise.val_);
+                promise.val_.reset();
+                return value;
+            }
+        }
+
+    private:
+        std::coroutine_handle<promise_type> handle_ = nullptr;
+    };
+
+    awaiter operator co_await() & noexcept { return awaiter{this->release()}; }
+    awaiter operator co_await() && noexcept { return awaiter{this->release()}; }
+
+    awaiter operator co_await() const& = delete;
+    awaiter operator co_await() const&& = delete;
 };
-
-template <class T> typename AwaitableTask<T>::awaiter AwaitableTask<T>::operator co_await() const { return awaiter{*this}; }
 
 } // namespace basiccoro
--- basic-coro/include/FfiAwaiter.hpp
+++ basic-coro/include/FfiAwaiter.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <atomic>
+#include <concepts>
 #include <coroutine>
+#include <cstddef>
+#include <exception>
+#include <mutex>
 #include <optional>
 #include <stdexcept>
 #include <type_traits>
@@ -10,100 +15,387 @@ namespace basiccoro {
 
 struct FfiCancelledException {};
 
-template <class T, class Initiator> class FfiAwaiter {
-public:
-    explicit FfiAwaiter(Initiator initiator) : initiator_(std::move(initiator)) {}
+namespace detail {
 
-    // Tell coroutine to always suspend when hitting co_await
+struct FfiStateBase {
+    std::atomic<std::size_t> refs{1};
+    std::mutex mutex;
+    std::coroutine_handle<> coro = nullptr;
+    std::exception_ptr exception_;
+    bool completed = false;
+    bool cancelled = false;
+    bool suspended = false;
+    bool ffi_armed = false;
+
+    void addRef() noexcept { refs.fetch_add(1, std::memory_order_relaxed); }
+
+    void releaseRef() noexcept {
+        if (refs.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+            delete this;
+        }
+    }
+
+    virtual ~FfiStateBase() = default;
+};
+
+template <class T> struct FfiState : public FfiStateBase {
+    std::optional<T> result_;
+};
+
+template <> struct FfiState<void> : public FfiStateBase {};
+
+inline void ffi_cancel_impl(FfiStateBase *state) {
+    if (!state) {
+        return;
+    }
+
+    std::coroutine_handle<> coro;
+    bool release_ffi_ref = false;
+
+    {
+        std::scoped_lock lock(state->mutex);
+
+        if (state->ffi_armed) {
+            state->ffi_armed = false;
+            release_ffi_ref = true;
+            state->cancelled = true;
+            state->completed = true;
+
+            if (state->suspended && state->coro) {
+                coro = state->coro;
+            }
+        }
+    }
+
+    if (coro) {
+        coro.resume();
+    }
+
+    if (release_ffi_ref) {
+        state->releaseRef();
+    }
+}
+
+template <class T, class U>
+    requires std::constructible_from<T, U&&>
+void ffi_resume_impl(FfiState<T> *state, U&& value) {
+    if (!state) {
+        return;
+    }
+
+    std::coroutine_handle<> coro;
+    bool release_ffi_ref = false;
+
+    {
+        std::scoped_lock lock(state->mutex);
+
+        if (state->ffi_armed) {
+            state->ffi_armed = false;
+            release_ffi_ref = true;
+            state->cancelled = false;
+            state->completed = true;
+
+            try {
+                state->result_.emplace(std::forward<U>(value));
+            } catch (...) {
+                state->exception_ = std::current_exception();
+            }
+
+            if (state->suspended && state->coro) {
+                coro = state->coro;
+            }
+        }
+    }
+
+    if (coro) {
+        coro.resume();
+    }
+
+    if (release_ffi_ref) {
+        state->releaseRef();
+    }
+}
+
+inline void ffi_resume_impl(FfiState<void> *state) {
+    if (!state) {
+        return;
+    }
+
+    std::coroutine_handle<> coro;
+    bool release_ffi_ref = false;
+
+    {
+        std::scoped_lock lock(state->mutex);
+
+        if (state->ffi_armed) {
+            state->ffi_armed = false;
+            release_ffi_ref = true;
+            state->cancelled = false;
+            state->completed = true;
+
+            if (state->suspended && state->coro) {
+                coro = state->coro;
+            }
+        }
+    }
+
+    if (coro) {
+        coro.resume();
+    }
+
+    if (release_ffi_ref) {
+        state->releaseRef();
+    }
+}
+
+} // namespace detail
+
+template <class T, class U>
+    requires std::constructible_from<T, U&&>
+void ffi_resume(void *opaque_handle, U&& value) {
+    detail::ffi_resume_impl(static_cast<detail::FfiState<T> *>(opaque_handle), std::forward<U>(value));
+}
+
+inline void ffi_resume(void *opaque_handle) { detail::ffi_resume_impl(static_cast<detail::FfiState<void> *>(opaque_handle)); }
+
+inline void ffi_cancel(void *opaque_handle) { detail::ffi_cancel_impl(static_cast<detail::FfiStateBase *>(opaque_handle)); }
+
+template <class T, class Initiator>
+    requires std::movable<T>
+class FfiAwaiter {
+public:
+    explicit FfiAwaiter(Initiator initiator) : initiator_(std::move(initiator)), state_(new detail::FfiState<T>()) {}
+
+    FfiAwaiter(const FfiAwaiter&) = delete;
+    FfiAwaiter& operator=(const FfiAwaiter&) = delete;
+    FfiAwaiter& operator=(FfiAwaiter&&) = delete;
+
+    FfiAwaiter(FfiAwaiter&& other) noexcept : initiator_(std::move(other.initiator_)), state_(std::exchange(other.state_, nullptr)) {}
+
+    ~FfiAwaiter() { reset(); }
+
+    // Tell coroutine to always go through await_suspend when hitting co_await
     bool await_ready() const noexcept { return false; }
 
-    // Called automatically by compiler when coroutine suspends
-    template <class Promise> void await_suspend(std::coroutine_handle<Promise> handle) {
-        coro_ = handle;
+    // Called automatically by compiler when coroutine reaches co_await
+    template <class Promise> bool await_suspend(std::coroutine_handle<Promise> handle) {
+        if (!state_) {
+            throw std::runtime_error("FfiAwaiter: no state");
+        }
+
+        auto *state = state_;
+
+        {
+            std::scoped_lock lock(state->mutex);
+            state->coro = handle;
+            state->suspended = false;
+            state->ffi_armed = true;
+        }
+
+        state->addRef();
+
         auto init = std::move(initiator_);
-        init(static_cast<void *>(this));
+        try {
+            init(static_cast<void *>(state));
+        } catch (...) {
+            std::scoped_lock lock(state->mutex);
+            state->coro = nullptr;
+            state->suspended = false;
+            throw;
+        }
+
+        std::scoped_lock lock(state->mutex);
+
+        if (state->completed) {
+            state->coro = nullptr;
+            state->suspended = false;
+            return false;
+        }
+
+        state->suspended = true;
+        return true;
     }
 
     // Called when coroutine resumes to unpack result
     T await_resume() {
-        if (cancelled_) {
+        if (!state_) {
+            throw std::runtime_error("FfiAwaiter: no state");
+        }
+
+        auto *state = state_;
+        std::exception_ptr exception;
+        std::optional<T> result;
+        bool cancelled = false;
+
+        {
+            std::scoped_lock lock(state->mutex);
+
+            if (!state->completed) {
+                throw std::runtime_error("FfiAwaiter: await_resume() before completion");
+            }
+
+            exception = state->exception_;
+            cancelled = state->cancelled;
+            state->coro = nullptr;
+            state->suspended = false;
+
+            if (!exception && !cancelled) {
+                if (!state->result_) {
+                    throw std::runtime_error("FfiAwaiter: no value in result_");
+                }
+
+                result.emplace(std::move(*state->result_));
+                state->result_.reset();
+            }
+        }
+
+        if (exception) {
+            std::rethrow_exception(exception);
+        }
+
+        if (cancelled) {
             throw FfiCancelledException();
         }
 
-        if constexpr (!std::is_same_v<T, void>) {
-            if (!result_) {
-                throw std::runtime_error("FfiAwaiter: no value in result_");
-            }
-            return std::move(*result_);
-        }
+        return std::move(*result);
     }
 
     // Static utility to be called when the FFI task completes
-    static void resume(void *opaque_handle, T value) {
-        auto *self = static_cast<FfiAwaiter *>(opaque_handle);
-        self->result_ = std::move(value);
-
-        if (self->coro_ && !self->coro_.done()) {
-            self->coro_.resume();
-        }
+    template <class U>
+        requires std::constructible_from<T, U&&>
+    static void resume(void *opaque_handle, U&& value) {
+        ffi_resume<T>(opaque_handle, std::forward<U>(value));
     }
 
-    // Static utility to destroy coroutine if FFI drops task
-    static void cancel(void *opaque_handle) {
-        auto *self = static_cast<FfiAwaiter *>(opaque_handle);
-        self->cancelled_ = true;
-
-        if (self->coro_ && !self->coro_.done()) {
-            self->coro_.resume();
-        }
-    }
+    // Static utility to signal cancellation if FFI drops task
+    static void cancel(void *opaque_handle) { ffi_cancel(opaque_handle); }
 
 private:
+    void reset() noexcept {
+        if (!state_) {
+            return;
+        }
+
+        {
+            std::scoped_lock lock(state_->mutex);
+            state_->coro = nullptr;
+            state_->suspended = false;
+        }
+
+        state_->releaseRef();
+        state_ = nullptr;
+    }
+
     Initiator initiator_;
-    std::coroutine_handle<> coro_ = nullptr;
-    std::optional<T> result_;
-    bool cancelled_ = false;
+    detail::FfiState<T> *state_ = nullptr;
 };
 
 // Specialization for void returns
 template <class Initiator> class FfiAwaiter<void, Initiator> {
 public:
-    explicit FfiAwaiter(Initiator initiator) : initiator_(std::move(initiator)) {}
+    explicit FfiAwaiter(Initiator initiator) : initiator_(std::move(initiator)), state_(new detail::FfiState<void>()) {}
+
+    FfiAwaiter(const FfiAwaiter&) = delete;
+    FfiAwaiter& operator=(const FfiAwaiter&) = delete;
+    FfiAwaiter& operator=(FfiAwaiter&&) = delete;
+
+    FfiAwaiter(FfiAwaiter&& other) noexcept : initiator_(std::move(other.initiator_)), state_(std::exchange(other.state_, nullptr)) {}
+
+    ~FfiAwaiter() { reset(); }
 
     bool await_ready() const noexcept { return false; }
 
-    template <class Promise> void await_suspend(std::coroutine_handle<Promise> handle) {
-        coro_ = handle;
+    template <class Promise> bool await_suspend(std::coroutine_handle<Promise> handle) {
+        if (!state_) {
+            throw std::runtime_error("FfiAwaiter: no state");
+        }
+
+        auto *state = state_;
+
+        {
+            std::scoped_lock lock(state->mutex);
+            state->coro = handle;
+            state->suspended = false;
+            state->ffi_armed = true;
+        }
+
+        state->addRef();
+
         auto init = std::move(initiator_);
-        init(static_cast<void *>(this));
+        try {
+            init(static_cast<void *>(state));
+        } catch (...) {
+            std::scoped_lock lock(state->mutex);
+            state->coro = nullptr;
+            state->suspended = false;
+            throw;
+        }
+
+        std::scoped_lock lock(state->mutex);
+
+        if (state->completed) {
+            state->coro = nullptr;
+            state->suspended = false;
+            return false;
+        }
+
+        state->suspended = true;
+        return true;
     }
 
     void await_resume() {
-        if (cancelled_) {
+        if (!state_) {
+            throw std::runtime_error("FfiAwaiter: no state");
+        }
+
+        auto *state = state_;
+        std::exception_ptr exception;
+        bool cancelled = false;
+
+        {
+            std::scoped_lock lock(state->mutex);
+
+            if (!state->completed) {
+                throw std::runtime_error("FfiAwaiter: await_resume() before completion");
+            }
+
+            exception = state->exception_;
+            cancelled = state->cancelled;
+            state->coro = nullptr;
+            state->suspended = false;
+        }
+
+        if (exception) {
+            std::rethrow_exception(exception);
+        }
+
+        if (cancelled) {
             throw FfiCancelledException();
         }
     }
 
-    static void resume(void *opaque_handle) {
-        auto *self = static_cast<FfiAwaiter *>(opaque_handle);
-        if (self->coro_ && !self->coro_.done()) {
-            self->coro_.resume();
-        }
-    }
+    static void resume(void *opaque_handle) { ffi_resume(opaque_handle); }
 
-    static void cancel(void *opaque_handle) {
-        auto *self = static_cast<FfiAwaiter *>(opaque_handle);
-        self->cancelled_ = true;
-
-        if (self->coro_ && !self->coro_.done()) {
-            self->coro_.resume();
-        }
-    }
+    static void cancel(void *opaque_handle) { ffi_cancel(opaque_handle); }
 
 private:
+    void reset() noexcept {
+        if (!state_) {
+            return;
+        }
+
+        {
+            std::scoped_lock lock(state_->mutex);
+            state_->coro = nullptr;
+            state_->suspended = false;
+        }
+
+        state_->releaseRef();
+        state_ = nullptr;
+    }
+
     Initiator initiator_;
-    std::coroutine_handle<> coro_ = nullptr;
-    bool cancelled_ = false;
+    detail::FfiState<void> *state_ = nullptr;
 };
 
 // Helper for template deduction, making it easy to invoke inline
